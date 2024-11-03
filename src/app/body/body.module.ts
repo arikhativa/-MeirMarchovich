c2c26aa25e244aa9b038695ddf323c034a29b23c
Arik Hativa <arikhativa@gmail.com>
2024-11-03 17:24:01 +0100

    basic header is done

--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -2,10 +2,11 @@ import { NavbarComponent } from './../navbar/navbar.component'
 import { NgModule } from '@angular/core'
 import { IntroComponent } from '../intro/intro.component'
 import { RouterModule } from '@angular/router'
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
+import { LogoComponent } from '../logo/logo.component'
 
 @NgModule({
-    declarations: [NavbarComponent, IntroComponent],
+    declarations: [NavbarComponent, IntroComponent, LogoComponent],
     imports: [RouterModule, CommonModule],
     exports: [IntroComponent, NavbarComponent],
 })
