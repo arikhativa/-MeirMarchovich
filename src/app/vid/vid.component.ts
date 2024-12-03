9a57dbece75f5bfb79f4dd88361a98bdb8b412e6
Yoav Rabby <yoavrbb@gmail.com>
2024-12-03 11:47:47 +0100

    added auto play in chrome added title to navbar

--- src/app/vid/vid.component.ts
+++ src/app/vid/vid.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-vid',
-  templateUrl: './vid.component.html',
-  styleUrl: './vid.component.scss'
+    selector: 'app-vid',
+    templateUrl: './vid.component.html',
+    styleUrl: './vid.component.scss',
 })
 export class VidComponent {
-
+    ngAfterViewInit() {
+        const vidElement = document.getElementById('video') as HTMLVideoElement
+        if (vidElement) {
+            vidElement.muted = true
+            vidElement.play()
+        }
+    }
 }
